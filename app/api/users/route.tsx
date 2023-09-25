f77c85bf0d40f5de51656cf5d22798347ef7b0f6
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-09-25 15:07:42 +0100

    API - Creating a user

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,5 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
-  return NextResponse.json({ msg: "Hello" });
+  return NextResponse.json([
+    { id: 1, name: "Mosh" },
+    { id: 2, name: "Abim" },
+  ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json({ err: "Name is compulosry" }, { status: 400 });
+  }
+  const randomID = Math.floor(Math.random() * 10);
+
+  return NextResponse.json({ id: randomID, name: body.name });
 }
